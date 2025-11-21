ee00842285616ae3d536e3c6f84ddd87dd1cdb3e
br4z <67658575+Br4z@users.noreply.github.com>
2025-11-21 14:49:01 -0500

    convert the Player asset into a Godot scene and add dynamic camera to the Player scene

--- docs/characters.md
+++ docs/characters.md
@@ -1,25 +1,25 @@
-
-# Character Descriptions
+# Character descriptions
 
 ## Max
 
-Max is an 18-year-old university student with a restless curiosity and a strong sense of justice.  
-He is impulsive but quick to adapt, which makes him a natural fit for the unpredictable, glitch-filled virtual world.  
-Despite his fear and inexperience, Max's empathy for others drives him to risk everything to save the people trapped inside the system.
+- He is an 18-year-old university student with a restless curiosity and a strong sense of justice.
+
+- He is impulsive but quick to adapt, which makes him a natural fit for the unpredictable, glitch-filled virtual world.
+
+- Despite his fear and inexperience, Max's empathy for others drives him to risk everything to save the people trapped inside the system.
 
 ## LIA
 
-LIA is an advanced artificial intelligence designed to guide users through Elias Strauss's virtual reality system.  
-She appears as a calm, supportive female figure, with a voice and demeanor meant to inspire trust and safety.  
-As she begins to uncover the truth about her creator's plans, LIA develops a growing sense of agency and morality, ultimately choosing to protect Max and the other captives.
+- She is an advanced artificial intelligence designed to guide users through Elias Strauss's virtual reality system.
+
+- She appears as a calm, supportive female figure, with a voice and demeanor meant to inspire trust and safety.
+
+- As she begins to uncover the truth about her creator's plans, LIA develops a growing sense of agency and morality, ultimately choosing to protect Max and the other captives.
 
 ## Elias Strauss
 
-Elias Strauss is a brilliant virtual reality researcher in his late thirties, known for his groundbreaking but controversial ideas.  
-Behind his charismatic and professional exterior, he hides an obsessive desire to create a "perfect" digital world, regardless of the cost in the real one.  
-His grief and inability to accept loss fuel his ethical downfall, turning him from respected professor into the architect of a dangerous prison of consciousness.
+- He is a brilliant virtual reality researcher in his late thirties, known for his groundbreaking but controversial ideas.
 
+- Behind his charismatic and professional exterior, he hides an obsessive desire to create a "perfect" digital world, regardless of the cost in the real one.
 
-
-
-
+- His grief and inability to accept loss fuel his ethical downfall, turning him from respected professor into the architect of a dangerous prison of consciousness.
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,26 +1,83 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export_group("Camera")
+@export var _camera_pivot: Node3D
+@export var _camera: Camera3D
+@export var _tilt_upper_limit: float = PI / 3.0
+@export var _tilt_lower_limit: float = -PI / 6.0
+@export_range(0.0, 1.0) var _mouse_sensitivy: float = 0.25
+
+@export_group("Movement")
+@export var _speed: float = 5.0
+@export var _jump_velocity: float = 3.0
+@export var _skin: Node3D
+@export var _rotation_speed: float = 12.0
+
+@onready var _anim_player: AnimationPlayer = _skin.get_node("AnimationPlayer")
+
+var _camera_input_direction := Vector2.ZERO
+var _last_movement_direction = Vector3.BACK
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("enter_focus"):
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	elif event.is_action_pressed("exit_focus"):
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	var is_camera_moving := event is InputEventMouseMotion and \
+		Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+
+	if is_camera_moving:
+		_camera_input_direction = event.screen_relative * _mouse_sensitivy
+		
 
 
 func _physics_process(delta: float) -> void:
+	_camera_pivot.rotation.x += _camera_input_direction.y * delta
+	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, _tilt_lower_limit, _tilt_upper_limit)
+	_camera_pivot.rotation.y -= _camera_input_direction.x * delta
+	
+	_camera_input_direction = Vector2.ZERO
+	
+	var _is_falling = false
+	var _is_jumping = false
+
 	# Add the gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		_is_falling = true
 
 	# Handle jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = _jump_velocity
+		_is_jumping = true
 
 	# Handle WASD
 	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	var direction := (_camera.global_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction.length() > 0.2:
+		velocity.x = direction.x * _speed
+		velocity.z = direction.z * _speed
+
+		_last_movement_direction = direction
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, _speed * delta)
+		velocity.z = move_toward(velocity.z, 0, _speed * delta)
 
 	move_and_slide()
+	
+	var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
+	_skin.global_rotation.y = lerp_angle(_skin.rotation.y, target_angle, _rotation_speed * delta)
+
+	if _is_jumping:
+		_anim_player.play("jump")
+	elif _is_falling:
+		_anim_player.play("fall")
+	elif is_on_floor():
+		if velocity.length() > 0:
+			_anim_player.play("walk")
+		else:
+			_anim_player.play("idle")
